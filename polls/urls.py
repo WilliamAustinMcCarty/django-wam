f02e998f601a61eb216c843380de41b07b546fc0
WilliamAustinMcCarty <78712326+WilliamAustinMcCarty@users.noreply.github.com>
2021-02-21 06:31:56 -0500

    turned ThoughtsView into nongeneric view
    I turned ThoughtsView into nongeneric view (new name thoughts). I also replaced the code in urls that were based on it being generic. The reason for this change was I was having trouble where the computer was having trouble reading what I was trying to get it to display.

--- polls/urls.py
+++ polls/urls.py
@@ -5,7 +5,8 @@ from . import views
 app_name = 'polls'
 urlpatterns = [
     path('', views.IndexView.as_view(), name='index'),
-    path('thoughts/', views.ThoughtsView.as_view(), name='thoughts'),
+    # path('thoughts/', views.ThoughtsView.as_view(), name='thoughts'),
+    path('thoughts/', views.thoughts, name='thoughts'),
     path('<int:pk>/', views.DetailView.as_view(), name='detail'),
     path('<int:pk>/results/', views.ResultsView.as_view(), name='results'),
     path('<int:question_id>/vote/', views.vote, name='vote'),
